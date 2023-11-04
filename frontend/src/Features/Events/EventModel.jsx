2196dfb53c50f94d603e15c3989006eed3fe5734
Rucha Kathar <49073201+RuchaRK@users.noreply.github.com>
2023-11-04 11:51:03 +0000

    Fix form array field error

--- frontend/src/Features/Events/EventModel.jsx
+++ frontend/src/Features/Events/EventModel.jsx
@@ -37,66 +37,65 @@ export const EventModel = ({ modalIsOpen, closeModal, handleSubmit, initialState
     <div>
       <Model isOpen={modalIsOpen} onRequestClose={closeModal}>
         <Formik initialValues={initialValues} onSubmit={onSubmit}>
-          {/* //  onSubmit={async (values) => {
-      //       handleSubmit(values);
-      //       closeModal();
-      //     }}> */}
-          <FormContainer>
-            <Title>New Event</Title>
-            <Form>
-              Name:
-              <Field type="text" name="eventName" />
-              Location:
-              <Field type="text" name="location" />
-              Description:
-              <Field type="text" name="description" />
-              Date:
-              <Field type="datetime-local" name="date" />
-              Role Specific Volunteers:
-              <FieldArray name="roleSpecificVolunteers">
-                {({ push, remove, values }) => {
-                  console.log(values);
-                  return (
-                    <div>
-                      {values?.roleSpecificVolunteers.map((_, index) => (
-                        <div key={index}>
-                          <Field
-                            type="text"
-                            name={`roleSpecificVolunteers[${index}].role`}
-                            placeholder="Role"
-                          />
-                          <Field
-                            type="number"
-                            name={`roleSpecificVolunteers[${index}].volunteers`}
-                            placeholder="No of Volunteers"
-                          />
-                          <button type="button" onClick={() => remove(index)}>
-                            Remove
+          {({values})=>{
+            return (
+              <Form>
+                <FormContainer>
+                  <Title>New Event</Title>
+                  Name:
+                  <Field type="text" name="eventName" />
+                  Location:
+                  <Field type="text" name="location" />
+                  Description:
+                  <Field type="text" name="description" />
+                  Date:
+                  <Field type="datetime-local" name="date" />
+                  Role Specific Volunteers:
+                  <FieldArray name="roleSpecificVolunteers">
+                    {({ push, remove }) => {
+                      console.log(values, initialValues);
+                      return (
+                        <div>
+                          {values.roleSpecificVolunteers.map((_, index) => (
+                            <div key={index}>
+                              <Field
+                                type="text"
+                                name={`roleSpecificVolunteers[${index}].role`}
+                                placeholder="Role"
+                              />
+                              <Field
+                                type="number"
+                                name={`roleSpecificVolunteers[${index}].volunteers`}
+                                placeholder="No of Volunteers"
+                              />
+                              <button type="button" onClick={() => remove(index)}>
+                                Remove
+                              </button>
+                            </div>
+                          ))}
+                          <button type="button" onClick={() => push({ role: '', volunteers: '' })}>
+                            Add Role-Specific Volunteer
                           </button>
                         </div>
-                      ))}
-                      <button type="button" onClick={() => push({ role: '', volunteers: '' })}>
-                        Add Role-Specific Volunteer
-                      </button>
-                    </div>
-                  );
-                }}
-              </FieldArray>
-              <ButtonContainer>
-                {/* <button
-                  disabled={wizardStatus === 'loading'}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleSubmit(values);
-                    closeModal();
-                  }}>
-                  {wizardStatus === 'loading' ? 'Submitting...' : 'Submit'}
-                </button>
-                <button onClick={closeModal}>close</button> */}
-                <button type="submit">Submit</button>
-              </ButtonContainer>
-            </Form>
-          </FormContainer>
+                      );
+                    }}
+                  </FieldArray>
+                  <ButtonContainer>
+                    {/* <button
+                      disabled={wizardStatus === 'loading'}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleSubmit(values);
+                        closeModal();
+                      }}>
+                      {wizardStatus === 'loading' ? 'Submitting...' : 'Submit'}
+                    </button>
+                    <button onClick={closeModal}>close</button> */}
+                    <button type="submit">Submit</button>
+                  </ButtonContainer>
+                </FormContainer>
+              </Form>)
+          }}
         </Formik>
       </Model>
     </div>
